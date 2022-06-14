d6f43bc95c210ed2d965a889304811afed43de04
taufe-mc <107316086+taufe-mc@users.noreply.github.com>
2022-06-14 21:50:35 +0900

    connect to discord
    Co-authored-by: Masanori Tani <masa@uta8a.net>

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+require('dotenv').config();
+const { Client, Intents } = require('discord.js'); //discord.js からClientとIntentsを読み込む
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] }); //clientインスタンスを作成する
+
+client.once('ready', () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
+	console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
+});
+
+client.login(process.env.AUTH_TOKEN); //ログインする
+
+
