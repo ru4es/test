3a0372e7b60f8d59e559911cc71a827e68f83806
taufe-mc <107316086+taufe-mc@users.noreply.github.com>
2022-06-16 21:40:06 +0900

    fetch weather api
    Co-authored-by: Masanori Tani <masa@uta8a.net>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-require('dotenv').config();
+import 'dotenv/config'
 import { Client, Intents, Interaction, TextChannel } from 'discord.js';
+import fetch from 'node-fetch';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+type WeatherResponse = {
+    daily: {
+        weathercode: Array<number>;
+    }
+}
 
 client.once('ready', async () => {
     console.log('起動完了');
-    await (client!.channels!.cache!.get(process.env.CHANNEL_ID!)! as TextChannel).send('明日の天気は')
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=34.3993&longitude=132.7140&daily=weathercode&timezone=Asia%2FTokyo')
+    const data = (await response.json()) as WeatherResponse;
+    const weathercode = data.daily.weathercode[1]
+    const rainJudge = [61, 63, 65, 66, 67, 80, 81, 82]
+    if (rainJudge.includes(weathercode)) {
+        await (client!.channels!.cache!.get(process.env.CHANNEL_ID!)! as TextChannel).send('明日の天気は')
+    }
     process.exit()
 });
 
